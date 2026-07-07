db9971c1f0df7f457a04407a26cce48f6f488457
Moses <mosesjayasinghp@gmail.com>
2026-07-07 12:19:14 +0530

    Completed Task 23

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import "./App.css";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="container">
+        <h1>Multi Page Website</h1>
+
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/contact">Contact</Link>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,10 @@
+function Contact() {
+  return (
+    <div>
+      <h2>Contact Page</h2>
+      <p>Email: support@example.com</p>
+    </div>
+  );
+}
+
+export default Contact;
